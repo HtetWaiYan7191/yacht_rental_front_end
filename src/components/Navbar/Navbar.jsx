8726c40f94075aba9bf13fe0d504e09ecef8c552
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-26 21:37:50 +0630

    Implement mobile responsie navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,37 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import {AiOutlineMenu} from 'react-icons/ai'
+import {RxCross1} from 'react-icons/rx'
+import './Navbar.css'
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    const newIsOpen = !isOpen;
+    setIsOpen(newIsOpen);
+    document.body.style.marginLeft = newIsOpen ? '0' : '0px';
+  };
+  
+
   return (
-    <div>
-      <h1>This is Navbar</h1>
+    <>
+    <div className={`sideNav ${isOpen ? 'w-[50%] transition z-30' : 'w-0'}`}>
+      <ul>
+        {/* <li className='p-3 text-5xl' onClick={toggleMenu}><RxCross1/></li> */}
+        <li className=''><NavLink to='/main' className='block px-4 text-[18px] py-4 font-semibold' onClick={toggleMenu}>Yachts</NavLink></li>
+        <li className=''><NavLink to='/reserve' className='block px-4 text-[18px] py-4 font-semibold' onClick={toggleMenu}>Reserve</NavLink></li>
+        <li className=''><NavLink to='/reservation' className='block px-4 text-[18px] py-4 font-semibold' onClick={toggleMenu}>My Reservations</NavLink></li>
+        <li className=''><NavLink to='/new/yacht' className='block px-4 text-[18px] py-4 font-semibold' onClick={toggleMenu}>Add Yacht</NavLink></li>
+        <li className=''><NavLink to='/delete/1/yacht ' className='block px-4 text-[18px] py-4 font-semibold' onClick={toggleMenu}>Delet Yacht</NavLink></li>
+      </ul>
+        
+
     </div>
+    <nav className={`p-[2rem] nav-container ${isOpen ? 'ms-[60%]' : 'ms-0'}`}>
+      <AiOutlineMenu onClick={toggleMenu} className='text-5xl'/>
+    </nav>
+    </>
   )
 }
 
