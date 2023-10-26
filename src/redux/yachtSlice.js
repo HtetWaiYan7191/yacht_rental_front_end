eb5b5691580f1d434d63d74e6296f75a68a6e9cc
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-26 21:38:28 +0630

    Implement basic structure for Main Page

--- src/redux/yachtSlice.js
+++ src/redux/yachtSlice.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     values: [],
@@ -7,11 +8,32 @@ const initialState = {
     error: ''
 }
 
+export const fetchYachts = createAsyncThunk('Main/yachts', async () => {
+    console.log('fetch function is working')
+    try {
+        const response = await axios.get('http://127.0.0.1:3000/yachts/')
+       
+        if(response.statusText !== 'OK') {
+            throw new Error('Can not fetch data')
+        }
+        return response.data
+        
+    } catch (error) {
+        throw new Error('Error fetching detail data');
+      }
+      
+})
+
 const yachtSlice = createSlice({
     name: 'yacht',
     initialState,
     reducers: {},
-    extraReducers: {}
+    extraReducers: (builder) => {
+        builder.addCase(fetchYachts.fulfilled, (state,action) => {
+            state.loading = false;
+            state.values = action.payload;
+        });
+    }
 })
 
 export default yachtSlice.reducer
