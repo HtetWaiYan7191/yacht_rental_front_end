533393f8c4320fe9d59078eabcfa9f8aa56e81ab
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-27 23:06:51 +0630

    Implement required components in delete page

--- src/pages/DeleteYacht.jsx
+++ src/pages/DeleteYacht.jsx
@@ -1,8 +1,15 @@
+import DeleteTable from "../components/DeleteTable/DeleteTable"
+
 const DeleteYacht = () => {
+  
   return (
-    <div>
-      delete yacht
-    </div>
+         <section className="delete-section lg:pt-20 pt-0">
+         <div className="delete-container  h-[100vh] lg:flex lg:flex-col lg:justify-end lg:h-[50vh] lg:items-center">
+           <h1 className="text-center text-2xl font-bold tracking-wider border-b-primary border-b-4 border-black w-[50%] lg:w-[15%] mx-auto pb-2">Delete Yachts</h1>
+           <DeleteTable/>
+         </div>
+       </section>
+   
   )
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,7 @@ import { fetchYachts } from "../redux/yachtSlice";
 import { useDispatch, useSelector } from "react-redux";
 import YachtCard from "../components/Yacht/YachtCard";
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import Loading from "../components/Loading/Loading";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import '../styles/Main.css'
@@ -16,39 +16,48 @@ import IconLink from "../components/IconLink/IconLink";
 const Main = () => {
   const dispatch = useDispatch();
   const yachts = useSelector((state) => state.yacht.values);
+  const loading = useSelector((state) => state.yacht.loading);
 
   useEffect(() => {
     dispatch(fetchYachts());
   }, [dispatch]);
 
   return (
-    <section className="main-section pt-10 flex justify-end ">
-      <main className="main-container w-[100%] lg:w-[80%] h-[115vh]">
-        <h1 className=" font-extrabold text-2xl lg:text-4xl text-center tracking-wider">LATEST MODELS</h1>
-        <h2 className="font-semibold  text-gray-500 text-center my-5">
-          Please select a Yacht
-        </h2>
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <section className="main-section pt-10 flex justify-end">
+          <main className="main-container w-[100%] lg:w-[80%] h-[115vh]">
+            <h1 className="font-extrabold text-2xl lg:text-4xl text-center tracking-wider">LATEST MODELS</h1>
+            <h2 className="font-semibold text-gray-500 text-center my-5">Please select a Yacht</h2>
 
-        <div className="mt-10">
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper"
-        spaceBetween={16} // Adjust the space between slides
-        slidesPerView={1} // Number of slides to show on initial load
-        breakpoints={{
-          // Define breakpoints for different screen sizes
-          768: {
-            slidesPerView: 3, // Show 3 slides on screens wider than 768px
-          },
-        }}>
-          {yachts?.map((yacht) => (
-            <SwiperSlide className="w-[70%] mx-auto" key={yacht.id}>
-            <YachtCard yacht={yacht} key={yacht.id} />
-            </SwiperSlide>
-          ))}
-       </Swiper>
-        </div>
-      </main>
-    </section>
+            <div className="mt-10">
+              <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                className="mySwiper"
+                spaceBetween={16}
+                slidesPerView={1}
+                breakpoints={{
+                  768: {
+                    slidesPerView: 3,
+                  },
+                }}
+              >
+                {yachts?.map((yacht) => (
+                  <SwiperSlide className="w-[70%] mx-auto" key={yacht.id}>
+                    <YachtCard yacht={yacht} key={yacht.id} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </main>
+        </section>
+      )}
+    </div>
   );
 };
+
 
 export default Main;
