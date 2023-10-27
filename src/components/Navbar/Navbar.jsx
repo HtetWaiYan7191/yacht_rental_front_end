be29f0076fdfd015fe0cb1e483776ce03ac6aabc
koppai <psthu.koppai@gmail.com>
2023-10-27 17:34:51 +0630

    Make Navbar only visible after authenticated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,40 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { logOut } from '../../redux/authenticationSlice';
+import { useDispatch } from 'react-redux';
 const Navbar = () => {
-  return (
-    <div>
-      <h1>This is Navbar</h1>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const token = sessionStorage.getItem('authToken');
+  const pathNames = ['/login', '/signup', '/'];
 
-export default Navbar
+  const handleLogout = () => {
+    dispatch(logOut(token)).then(() => {
+      sessionStorage.clear();
+      navigate('/login');
+    });
+  };
+
+  if (pathNames.includes(location.pathname)) {
+    return null;
+  }
+
+  return (
+    <div className={`${token ? 'block' : 'hidden'}`}>
+      <h1>This is Navbar</h1>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              handleLogout();
+            }}
+          >
+            Sign Out
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
