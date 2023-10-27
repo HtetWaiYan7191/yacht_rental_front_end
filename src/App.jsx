2d2bccc5d8b46404b169de619e17f1e4fce30daf
Yout <youtlone95@gmail.com>
2023-10-27 17:21:57 +0630

    Update of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
-import Detail from './pages/Detail'
+import Detail from './components/Detail/Detail'
 import Reservation from './pages/Reservation'
 import ErrorPage from './pages/ErrorPage'
 import React from 'react'
@@ -9,7 +9,7 @@ import React from 'react'
 const App = () => (
   <Routes>
     <Route path="/" element={<Main/>}></Route>
-    <Route path="/detail/:id" element={<Detail/>}></Route>
+    <Route path="/details/:id" element={<Detail/>}></Route>
     <Route path="/reservation" element={<Reservation/>}></Route>
     <Route path="*" element={<ErrorPage/>}> </Route>
   </Routes>
