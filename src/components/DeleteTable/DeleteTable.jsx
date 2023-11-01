f46843094a4bf932f5b705743b0d8c4e8661ea17
Yout <youtlone95@gmail.com>
2023-11-01 12:26:08 +0630

    Update of Delete Table width in desktop version

--- src/components/DeleteTable/DeleteTable.jsx
+++ src/components/DeleteTable/DeleteTable.jsx
@@ -3,28 +3,30 @@ import PropTypes from 'prop-types'
 
 const DeleteTable = ({yachts, handleClick}) => {
     return (
-            <table className="table w-[90%] lg:w-[40%] mx-auto border-2 mt-10">
-              <thead>
-                <tr className="border-2 bg-primary/80 text-white">
-                  <th className="p-3 font-semibold text-md">Name</th>
-                  <th className="p-3 font-semibold text-md">Model</th>
-                  <th className="p-3 font-semibold text-md">Fee Per Day</th>
-                  <th className="p-3 font-semibold text-md">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {yachts.map((yacht) => (
-                  <tr key={yacht.id} className="table-row">
-                    <td className="text-center border-b py-3 text-sm">{yacht.name}</td>
-                    <td className="text-center border-b py-3 text-sm">{yacht.model}</td>
-                    <td className="text-center border-b py-3 text-sm">$ {yacht.fee_per_day}</td>
-                    <td className="text-center border-b border-b-white py-3 text-sm bg-red-500 text-white">
-                      <button onClick={() => handleClick(yacht.id)}>Delete</button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>    
+      <div className="w-full mx-auto mt-10">
+      <table className="table w-full lg:w-[80%] mx-auto border-2">
+        <thead>
+          <tr className="border-2 bg-primary/80 text-white">
+            <th className="p-3 font-semibold text-md">Name</th>
+            <th className="p-3 font-semibold text-md">Model</th>
+            <th className="p-3 font-semibold text-md">Fee Per Day</th>
+            <th className="p-3 font-semibold text-md">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {yachts.map((yacht) => (
+            <tr key={yacht.id} className="table-row">
+              <td className="text-center border-b py-3 text-sm">{yacht.name}</td>
+              <td className="text-center border-b py-3 text-sm">{yacht.model}</td>
+              <td className="text-center border-b py-3 text-sm">$ {yacht.fee_per_day}</td>
+              <td className="text-center border-b border-b-white py-3 text-sm bg-red-500 text-white">
+                <button onClick={() => handleClick(yacht.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>   
+      </div> 
   )
 }
 
